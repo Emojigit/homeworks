bcfab34f89bbdce5e95515d32ff2966a2b5ad2d5
Emoji <55009343+Emojigit@users.noreply.github.com>
2022-10-04 20:11:57 +0800

    50 name

--- homeworks/main/models.py
+++ homeworks/main/models.py
@@ -6,7 +6,7 @@ import datetime
 # Create your models here.
 
 class Subject(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     short_code = models.CharField(max_length=10)
     sort_key = models.CharField(max_length=10)
     def __str__(self):
