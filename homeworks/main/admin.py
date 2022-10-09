c5ba8ac60709af89c20a063296554df02bde971b
Emoji <55009343+Emojigit@users.noreply.github.com>
2022-10-09 10:49:34 +0800

    admin sorting

--- homeworks/main/admin.py
+++ homeworks/main/admin.py
@@ -4,12 +4,14 @@ from main import models
 # Register your models here.
 
 class SubjectAdmin(admin.ModelAdmin):
-    list_display = ('sort_key','short_code','name' )
+    list_display = ('sort_key','short_code','name',)
+    ordering = ('sort_key',)
 
 admin.site.register(models.Subject,SubjectAdmin)
 
 class HomeworkAdmin(admin.ModelAdmin):
-    list_display = ('subject','description','type','create_date','due_date' )
-    list_filter = ('subject','type','create_date','due_date' )
+    list_display = ('subject','description','type','create_date','due_date',)
+    list_filter = ('subject','type','create_date','due_date',)
+    ordering = ('create_date','due_date','subject__sort_key','description',)
 
 admin.site.register(models.Homework,HomeworkAdmin)
